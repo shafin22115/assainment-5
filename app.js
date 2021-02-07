bfe89356c62b296c037ea88267876b87386d8f30
shafin22115 <shafin22115@gmail.com>
2021-02-07 20:11:31 +0600

    secendPart

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const searchBtn = document.getElementById('search-button')
 const foodLIst = document.getElementById('food');
 
+// addEventListener for search btn
 searchBtn.addEventListener('click', getMealList);
-
+// fumction for meal list
 function getMealList(){
     let searchInputTxt = document.getElementById('food-name').value;
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
@@ -16,56 +17,15 @@ function getMealList(){
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h3> ${meal.strMeal}</h3>
+                        <button> View Detail</button>
                     </div>
                     </div>
-                `
+               `
             })
+        }else{
+            html = "sorry, we didn't find any meal"
         }
         foodLIst.innerHTML = html
     })
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const listItem = document.getElementById('food')
-// const food = document.getElementById('food-name').value.trim()
-// const getFoodName = () =>{
-   
-//     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${food}`)
-// .then(res => res.json())
-// .then(data => {
-//     let html = "";
-//     if(data.meals){
-//         data.meals.forEach(meal => {
-//             html +=`
-//             <div>
-//                     <div class="card">
-//                     <img src=" ${meal.strMealThumb}">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${meal.strMeal}</h5>
-                        
-//                     </div>
-//                     </div>
-//             </div>`
-//         })
-//     }
-//     listItem.innerHTML = html;
-// })
-// }
-
-// const searchBtn = document.getElementById("search-button");
-// searchBtn.addEventListener('click', () => {
-//     let inputFood = document.getElementById("food-name").value;
-//     getFoodName(inputFood)
-// })
